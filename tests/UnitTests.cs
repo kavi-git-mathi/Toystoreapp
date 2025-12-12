b4cc0654143f42286d49d313976a5dc545d735ed
kavi-git-mathi <mathi.vanan612@outlook.com>
2025-12-11 20:53:43 -0500

    test file added

---
+++ tests/UnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ToyStoreApp.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
